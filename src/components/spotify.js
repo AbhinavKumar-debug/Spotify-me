73c78a12f689ffd3cce2b0e93c2dd195132ab0a1
AbhinavKumar-debug <abhinav1445.be20@chitkara.edu.in>
2022-09-16 06:27:37 +0530

    Login page

---
+++ src/components/spotify.js
@@ -0,0 +1,15 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const redirectUrl = "https://localhost:3000/";
+const clientId = "11ebfca8cb46462cb187e5ef139ec8f6";
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl =`${authEndPoint}?client_id=${clientId}&redirect_url=${redirectUrl}&scopes=${scopes.join("%20")}&response_type=token&show_dialog=true `
