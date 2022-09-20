b1ed796a256182d066587a82e07a730998961cfe
AbhinavKumar-debug <abhinav1445.be20@chitkara.edu.in>
2022-09-20 14:31:51 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 import Login from './components/Login';
+import {getTokenFromUrl} from "./components/spotify"
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const[token, setToken] = useState(null);
+
+  useEffect(() =>{
+    // set token
+    const hash = getTokenFromUrl();
+    window.location.hash="";
+    const _token =hash.access_token;
+
+    if(_token){
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user =>{
+        console.log('userOn',user);
+      });
+    }
+
+    console.log("I have a token => ",token);
+    
+  },[]);
   return (
     <div className='app'>
-      <Login/>
+      {
+        token ? (
+          // <Player />
+          <h1>I am logged in</h1>
+        ):(
+          <Login />
+        )
+      }
+
     </div>
   );
 }
--- src/components/spotify.js
+++ src/components/spotify.js
@@ -11,5 +11,17 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ];
+export const getTokenFromUrl =()=>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial,item) => {
+        var parts = item.split('=')
+        initial[parts[0]]=decodeURIComponent(parts[1])
+
+        return initial;
+    },{});
+}
+
 
 export const loginUrl =`${authEndPoint}?client_id=${clientId}&redirect_url=${redirectUrl}&scopes=${scopes.join("%20")}&response_type=token&show_dialog=true `
